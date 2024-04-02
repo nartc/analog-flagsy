71a7e001afb83e64199995cfe03ec22eec64b09d
Chau Tran <nartc7789@gmail.com>
2024-04-02 12:37:09 +0700

    add init

--- src/app/pages/index.server.ts
+++
@@ -1,8 +0,0 @@
-import type { PageServerLoad } from '@analogjs/router';
-import { setCookie } from 'h3';
-import { prisma } from '../../server/prisma';
-
-export const load = async ({ event }: PageServerLoad) => {
-	setCookie(event, 'test', 'foo');
-	return await prisma.user.findMany();
-};
