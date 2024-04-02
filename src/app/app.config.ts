5be8d04c027dbf01a61f022a794039950420dda5
Chau Tran <nartc7789@gmail.com>
2024-04-02 11:11:49 +0700

    init

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,13 @@
+import { provideFileRouter } from '@analogjs/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideFileRouter } from '@analogjs/router';
+import { withComponentInputBinding } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideFileRouter(),
-    provideHttpClient(withFetch()),
-    provideClientHydration(),
-  ],
+	providers: [
+		provideFileRouter(withComponentInputBinding()),
+		provideHttpClient(withFetch()),
+		provideClientHydration(),
+	],
 };
